73c4032cb403d03c28d2ba64611141222629f194
Pikowal <piotr.jakub.kowal@gmail.com>
2019-06-03 20:21:58 +0200

    added score and lives text

---
+++ Arcanoid/Assets/Scripts/BlockScript.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    public int points;
+}
---
+++ Arcanoid/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int lives;
+    public int score;
+    public Text scoreText;
+    public Text liveText;
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateLives(int changeInLives)
+    {
+        lives += changeInLives;
+
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+
+        liveText.text = $"Lives: {lives}";
+    }
+   
+    public void UpdateScores(int changeScore)
+    {
+        score += changeScore;
+        scoreText.text = $"Score: {score}";
+    }
+
+    private void Init()
+    {
+        liveText.text = $"Lives: {lives}";
+        scoreText.text = $"Score: {score}";
+    }
+
+    private void GameOver()
+    {
+        gameOver = true;
+    }
+
+}
